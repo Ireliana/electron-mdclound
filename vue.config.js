5ffe024245324bfbfdf6e00e3d313fba2ecff905
kuangmincong <562385633@qq.com>
2020-04-17 18:44:00 +0800

    init

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+const path = require("path");
+function resolve(dir) {
+	return path.resolve(__dirname, dir);
+}
+
+module.exports = {
+	chainWebpack: (config) => {
+		config.resolve.alias.set("src", resolve("src"));
+	},
+};
